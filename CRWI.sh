0e011648b8c72ed0b53ed249b3c6713674648691
Krzysztof Ruczkowski <tojatos@gmail.com>
2018-10-28 00:44:31 +0200

    CRWI path change

--- CRWI.sh
+++ CRWI.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SPOJ_PATH="/home/tojatos/Scripts/cppPolitechnika/"
+SPOJ_PATH="/home/tojatos/Scripts/KNCPP2018/"
 SAVE_PATH="/tmp/"
 g++ $SPOJ_PATH$1 -o $SAVE_PATH$1
 $SAVE_PATH$1 < "$SPOJ_PATH"input.txt 
