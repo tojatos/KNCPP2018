03817fa50768e9e6569e6bc65278be434c5d53a3
Krzysztof Ruczkowski <tojatos@gmail.com>
2018-11-24 02:17:52 +0100

    Refactoring and preparation

--- AI/WondevWoman.cpp
+++ AI/WondevWoman.cpp
@@ -11,50 +11,74 @@ struct LegalAction {
 	string dir1;
 	string dir2;
 };
+struct Coordinate{
+	int x;
+	int y;
+};
 
+// class GameState {
+// public:
+// 	vector<int> board;
+// };
+
+vector<int> getBoard(int size) {
+	vector<int> board;
+	for (int i = 0; i < size; i++) {
+		string row;
+		cin >> row; cin.ignore();
+		for(int j = 0; j < size; j++) {
+			switch (row[j]) {
+				case '.': board.push_back(-1); break;
+				default:
+					int x = row[j] - '0';
+					board.push_back(x);
+					break;
+			}
+		}
+	}
+	return board;
+}
+vector<Coordinate> getCoordinates(int unitsPerPlayer) {
+	vector<Coordinate> coordinates;
+	for (int i = 0; i < unitsPerPlayer; i++) {
+		Coordinate c;
+		cin >> c.x >> c.y; cin.ignore();
+		coordinates.push_back(c);
+	}
+	return coordinates;
+}
+
+vector<LegalAction> getLegalActions(int legalActionsNum){
+	vector<LegalAction> legalActions;
+	for (int i = 0; i < legalActionsNum; i++) {
+		LegalAction a;
+		cin >> a.atype >> a.index >> a.dir1 >> a.dir2; cin.ignore();
+		legalActions.push_back(a);
+	}
+	return legalActions;
+}
+void cerrBoard(vector<int> board, int size) {
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			cerr << board[i*size + j] << ' ';
+		}
+		cerr << endl;
+	}
+}
 int main()
 {
-	const int range_from  = 0;
-	const int range_to    = 10;
-	std::random_device                  rand_dev;
-	std::mt19937                        generator(rand_dev());
-	std::uniform_int_distribution<int>  distr(range_from, range_to);
-	int size;
-	cin >> size; cin.ignore();
-	int unitsPerPlayer;
-	cin >> unitsPerPlayer; cin.ignore();
+	int size; cin >> size; cin.ignore();
+	int unitsPerPlayer; cin >> unitsPerPlayer; cin.ignore();
 
 	// game loop
 	while (1) {
-		for (int i = 0; i < size; i++) {
-			string row;
-			cin >> row; cin.ignore();
-		}
-		for (int i = 0; i < unitsPerPlayer; i++) {
-			int unitX;
-			int unitY;
-			cin >> unitX >> unitY; cin.ignore();
-		}
-		for (int i = 0; i < unitsPerPlayer; i++) {
-			int otherX;
-			int otherY;
-			cin >> otherX >> otherY; cin.ignore();
-		}
-		int legalActionsNum;
-		cin >> legalActionsNum; cin.ignore();
-
-		vector<LegalAction> legalActions;
-		for (int i = 0; i < legalActionsNum; i++) {
-			LegalAction a;
-			cin >> a.atype >> a.index >> a.dir1 >> a.dir2; cin.ignore();
-			legalActions.push_back(a);
-		}
+		vector<int> board = getBoard(size);
+		vector<Coordinate> allyCoordinates = getCoordinates(unitsPerPlayer);
+		vector<Coordinate> enemyCoordinates = getCoordinates(unitsPerPlayer);
+		int legalActionsNum; cin >> legalActionsNum; cin.ignore();
+		vector<LegalAction> legalActions = getLegalActions(legalActionsNum);
 		cout << legalActions[0].atype << ' ' << legalActions[0].index << ' ' << legalActions[0].dir1 << ' ' << legalActions[0].dir2 << endl;
-
-		// Write an action using cout. DON'T FORGET THE "<< endl"
-		// To debug: cerr << "Debug messages..." << endl;
-		// while(1) {
-		// cout << "MOVE&BUILD 0 N S" << endl;
-		// }
 	}
 }
