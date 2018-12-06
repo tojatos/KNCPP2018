892959c21a0b3a7682e6aa3c72c07d1cab071643
Krzysztof Ruczkowski <tojatos@gmail.com>
2018-12-07 00:00:10 +0100

    STO3 working but too slow

---
+++ Algorithms/STO3.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int min(int a, int b) { return a < b ? a : b; }
+int main()
+{
+	int n, m; cin >> n >> m;
+	int arr[n][m];
+	int ans[n][m];
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			cin >> arr[i][j];
+		}
+	}
+	for (int j = m - 1; j >= 0; --j) {
+		for (int i = 0; i < n; ++i) {
+			if(j==m - 1) ans[i][j] = arr[i][j];
+			else {
+				int mini = INT_MAX;
+
+				if(i > 0) mini = min(mini, ans[i-1][j+1]);
+				mini = min(mini, ans[i][j+1]);
+				if(i < n - 1) mini = min(mini, ans[i+1][j+1]);
+
+				ans[i][j] = arr[i][j] + mini;
+			}
+		}
+		if(j != m-1) {
+			bool playLoop = true;
+			while(playLoop) {
+				playLoop = false;
+				for (int i = 0; i < n; ++i) {
+					int mini = ans[i][j] - arr[i][j];
+					int tmp = mini;
+
+					if(i > 0) mini = min(mini, ans[i-1][j]);
+					if(i < n - 1) mini = min(mini, ans[i+1][j]);
+
+					ans[i][j] = arr[i][j] + mini;
+
+					if(tmp!=mini) playLoop = true;
+				}
+			}
+		}
+	}
+
+	// for (int i = 0; i < n; ++i) {
+	// 	for (int j = 0; j < m; ++j) {
+	// 		cout << ans[i][j] << ' ';
+	// 	}
+	// 	cout << endl;
+	// }
+
+	int mini = INT_MAX;
+	for (int i = 0; i < n; ++i) {
+		mini = min(mini, ans[i][0]);
+	}
+	cout << mini <<  endl;
+	return 0;
+}
