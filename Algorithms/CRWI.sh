8001efff877540a405b243a970c17af98b0f455e
tojatos <tojatos@gmail.com>
2018-12-30 22:31:41 +0100

    C++ 11 compiling

--- Algorithms/CRWI.sh
+++ Algorithms/CRWI.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SPOJ_PATH="/home/tojatos/Scripts/KNCPP2018/Algorithms/"
 SAVE_PATH="/tmp/"
-g++ $SPOJ_PATH$1 -o $SAVE_PATH$1
+g++ -std=c++11 $SPOJ_PATH$1 -o $SAVE_PATH$1
 time $SAVE_PATH$1 < "$SPOJ_PATH"input.txt 
